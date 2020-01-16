0184772a03f1e93c9ebd87da7e0fd64a2c2bd94b
lofisoul <jason.nolfi@gmail.com>
2020-01-16 11:21:19 -0600

    Update to s3 utility with npm scripts to deploy

--- s3-upload.js
+++ s3-upload.js
@@ -13,34 +13,77 @@ const s3 = new AWS.S3({
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-//set array of files
-const fileArray = [
-    {
-        filename: process.env.CSS_FILENAME,
-        s3Path: process.env.CSS_S3_PATH, 
-        localfilepath: process.env.CSS_LOCAL_PATH, 
-        contentType: 'text/css',
-        type: 'css'
-    },
-    {
-        filename: process.env.JS_FILENAME, 
-        s3Path: process.env.JS_S3_PATH,
-        localfilepath: process.env.JS_LOCAL_PATH,
-        contentType: 'application/javascript',
-        type: 'js'
-    },
-    {
-        filename: 'loginbundle.js', 
-        s3Path: process.env.JS_S3_PATH,
-        localfilepath: './public/js/loginbundle.js',
-        contentType: 'application/javascript',
-        type: 'js'
+//object constructor for file objects
+function fileObject(filename,s3Path,localfilepath,type) {
+    this.filename = filename;
+    this.s3Path = s3Path;
+    this.localfilepath = localfilepath;
+
+    if (type === 'css') {
+        this.contentType = 'text/css',
+        this.type = 'css'
+    } 
+    
+    if (type === 'js') {
+        this.contentType = 'application/javascript',
+        this.type = 'js'
     }
-]
+
+}
+//generate file array for s3
+function genS3ArgArray(args) {
+    const fileArray = [];
+    if (args.length<=0) {
+        let fileCSS = new fileObject(process.env.CSS_FILENAME,process.env.CSS_S3_PATH,process.env.CSS_LOCAL_PATH,'css');
+        let fileJS = new fileObject(process.env.JS_FILENAME,process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+        let fileLogin = new fileObject('loginbundle.js',process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+        fileArray.push(fileCSS, fileJS, fileLogin);
+    } else {
+        //nothing = akk //css = cssfile //axisbundle = axisbundlejs // loginbundle = loginbundlejs // axis = css and axisbundle
+        args.forEach(function (val) {
+            switch(val) {
+                case 'css': {
+                    let file = new fileObject(process.env.CSS_FILENAME,process.env.CSS_S3_PATH,process.env.CSS_LOCAL_PATH,'css');
+                    fileArray.push(file);
+                    }
+                break;
+                case 'axisbundle': {
+                    let file = new fileObject(process.env.JS_FILENAME,process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+                    fileArray.push(file);
+                } 
+                break;
+                case 'loginbundle': {
+                    let file = new fileObject('loginbundle.js',process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+                    fileArray.push(file);
+                }
+                break;
+                case 'axis': {
+                    let fileCSS = new fileObject(process.env.CSS_FILENAME,process.env.CSS_S3_PATH,process.env.CSS_LOCAL_PATH,'css');
+                    let fileJS = new fileObject(process.env.JS_FILENAME,process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+                    fileArray.push(fileCSS, fileJS);
+                }
+                break; 
+                default: {
+                    let fileCSS = new fileObject(process.env.CSS_FILENAME,process.env.CSS_S3_PATH,process.env.CSS_LOCAL_PATH,'css');
+                    let fileJS = new fileObject(process.env.JS_FILENAME,process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+                    let fileLogin = new fileObject('loginbundle.js',process.env.JS_S3_PATH,process.env.JS_LOCAL_PATH,'js');
+                    fileArray.push(fileCSS, fileJS, fileLogin);
+                }
+            }
+        });
+    }
+    //console.log(fileArray);
+    return fileArray
+}
+
+//set args
+const args = process.argv.slice(2);
+//set array of files
+const fileArray = genS3ArgArray(args);
 
 //function to upload array of files
 fileArray.map(file => {
-    const fileContent = fs.readFileSync(file.localfilepath);
+    const fileContent = fs.readFileSync(file.localfilepath+'/'+file.filename);
     
     const params = {
         Bucket: process.env.BUCKET_NAME,
